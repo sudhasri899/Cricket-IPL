2e5cd95cc1808046434d1b2e2bed3b21c9f02bea
Sudha Sri <psudhasri@stratapps.com>
2021-11-21 14:49:12 +0530

    First Commit

---
+++ Cricket-IPL/src/main/java/com/example/demo/CricketIplApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketIplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketIplApplication.class, args);
+	}
+
+}
