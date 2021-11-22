c99b2fd312774babac988a77d5fcc68f702c34c9
Sudha Sri <psudhasri@stratapps.com>
2021-11-22 10:42:03 +0530

    First Commit

--- Cricket-IPL/src/main/java/com/example/entity/Role.java
+++ Cricket-IPL/src/main/java/com/example/entity/Role.java
@@ -1,26 +1,32 @@
 package com.example.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name="Roles")
+@Table(name="roles")
 @Getter
 @Setter
 public class Role {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="roleid")
+	@Column(name="role_id")
 	private long roleId;
 	
-	@Column(name="rolename")
+	@Column(name="role_name")
 	private String roleName;
+	
+	@ManyToMany
+	private List<User> user;
 }
--- Cricket-IPL/src/main/java/com/example/entity/User.java
+++ Cricket-IPL/src/main/java/com/example/entity/User.java
@@ -1,35 +1,46 @@
 package com.example.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name="SuperAdmin")
+@Table(name="superadmin")
 @Getter
 @Setter
 public class User {
  
 	@Id
-	@Column(name="userid")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="user_id")
 	private long userId;
 	
-	@Column(name="firstname")
+	@Column(name="first_name")
 	private String firstName;
 	
-	@Column(name="lastname")
+	@Column(name="last_name")
 	private String lastName;
 	
-	@Column(name="emailid")
+	@Column(name="email_id")
 	private String emailId;
 	
-	@Column(name="username")
+	@Column(name="user_name")
 	private String userName;
 	
 	@Column(name="password")
 	private String password;
+	
+	@ManyToMany
+	@JoinColumn
+	private List<Role> role;
 }
